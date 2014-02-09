b03a45ed047d20247157a8a04bc4ecc342789768
Rafael Portela <rafaelportela90@gmail.com>
2014-02-09 12:45:16 -0200

    [aiplan] The Puzzle problem.
    - Puzzle data
    - Compare which of two states is closest to goal
    - Generates further movements from a given state

---
+++ aiplan/puzzle.py
@@ -0,0 +1,137 @@
+class Puzzle:
+
+  def __init__(self, state):
+    self.state = state
+
+  def distanceToGoal(self):
+    m = Movement()
+    moves = 0
+
+    for i in range(0, 9):
+      moves += m.countMoves(self.state[i], i)
+
+    return moves
+
+class Movement:
+
+  def countMoves(self, piece, position):
+    num = int(piece)
+
+    targetVPos = num % 3
+    targetHPos = int(num / 3) 
+
+    currentVPos = position % 3
+    currentHPos = int(position / 3)
+
+    vMoves = abs(targetVPos - currentVPos) 
+    hMoves = abs(targetHPos - currentHPos) 
+
+    return vMoves + hMoves
+
+class PuzzleStateComparator:
+
+  def compare(self, aPuzzle, otherPuzzle):
+    d1 = aPuzzle.distanceToGoal()
+    d2 = otherPuzzle.distanceToGoal()
+
+    if d1 > d2:
+      return 1
+    elif d1 < d2:
+      return -1
+    else:
+      return 0
+
+class Generator:
+
+  def statesFrom(self, current):
+    zeroIndex = current.state.rfind('0')
+    possibleStates = []
+
+    if self.canZeroMoveUp(zeroIndex):
+      possibleStates.append(self.moveZeroUp(current.state))
+
+    if self.canZeroMoveDown(zeroIndex):
+      possibleStates.append(self.moveZeroDown(current.state))
+
+    if self.canZeroMoveRight(zeroIndex):
+      possibleStates.append(self.moveZeroRight(current.state))
+
+    if self.canZeroMoveLeft(zeroIndex):
+      possibleStates.append(self.moveZeroLeft(current.state))
+
+    return possibleStates
+
+  def canZeroMoveUp(self, zeroIndex):
+    if self.row(zeroIndex) > 0:
+      return True
+    else:
+      return False
+
+  def canZeroMoveDown(self, zeroIndex):
+    if self.row(zeroIndex) < 2:
+      return True
+    else:
+      return False
+
+  def canZeroMoveRight(self, zeroIndex):
+    if self.column(zeroIndex) < 2:
+      return True
+    else:
+      return False
+
+  def canZeroMoveLeft(self, zeroIndex):
+    if self.column(zeroIndex) > 0:
+      return True
+    else:
+      return False
+
+  def moveZeroUp(self, state):
+    index = state.rfind('0')
+    target = index - 3
+
+    return \
+        state[0:target] + \
+        state[index] + \
+        state[target+1:index] + \
+        state[target] + \
+        state[index+1::]
+
+  def moveZeroDown(self, state):
+    index = state.rfind('0')
+    target = index + 3
+
+    return \
+        state[0:index] + \
+        state[target] + \
+        state[index+1:target] + \
+        state[index] + \
+        state[target+1::]
+  
+  def moveZeroRight(self, state):
+    index = state.rfind('0')
+    target = index + 1
+
+    return \
+        state[0:index] + \
+        state[target] + \
+        state[index+1:target] + \
+        state[index] + \
+        state[target+1::]
+
+  def moveZeroLeft(self, state):
+    index = state.rfind('0')
+    target = index - 1
+
+    return \
+        state[0:target] + \
+        state[index] + \
+        state[target+1:index] + \
+        state[target] + \
+        state[index+1::]
+
+  def row(self, index):
+    return int(index / 3)
+
+  def column(self, index):
+    return int(index % 3)
+
---
+++ aiplan/puzzleTest.py
@@ -0,0 +1,124 @@
+import unittest
+from puzzle import Puzzle
+
+class PuzzleTest(unittest.TestCase):
+
+  def testGoalStateHasNoDistance(self):
+    p = Puzzle("012345678")
+    self.assertEqual(p.distanceToGoal(), 0)
+
+  def testHowManyMovesIsCurrentStateFromGoal(self):
+    p = Puzzle("021345678")
+    self.assertEqual(p.distanceToGoal(), 2)
+
+from puzzle import Movement
+
+class MovementTest(unittest.TestCase):
+
+  def testHowManyMovesIsPieceFromCorrentPlace(self):
+    m = Movement()
+    piece_number = '8'
+    current_position = 0
+    moves = m.countMoves(piece_number, current_position)
+
+    self.assertEqual(moves, 4)
+
+from puzzle import PuzzleStateComparator
+
+class PuzzleStateComparatorTest(unittest.TestCase):
+
+  def testIfStateIsCloserToGoalThanOther(self):
+    p1 = Puzzle("120345678")
+    p2 = Puzzle("102345678")
+
+    c = PuzzleStateComparator()
+    result = c.compare(p1, p2)
+
+    self.assertEqual(result, 1)
+
+  def testDifferentStatesCanHaveSameDistanceToGoal(self):
+    p1 = Puzzle("120345678")
+    p2 = Puzzle("012345867")
+
+    comparator = PuzzleStateComparator()
+    result = comparator.compare(p1, p2)
+
+    self.assertEqual(result, 0)
+
+from puzzle import Generator
+
+class GeneratorClass(unittest.TestCase):
+
+  def testGeneratePossibleStatesFromCurrent(self):
+    current = Puzzle("123456780")
+    g = Generator()
+
+    possibleStates = g.statesFrom(current)
+    self.assertEqual(len(possibleStates), 2)
+
+  def testIfStateCanMoveZeroUp(self):
+    """ 123456780 has 0 in the last row, it can move up """
+    zeroInLastRow = 8
+    g = Generator()
+
+    canMoveUp = g.canZeroMoveUp(zeroInLastRow)
+    self.assertEqual(canMoveUp, True)
+
+  def testIfStateCanMoveZeroDown(self):
+    """ 123405678 has 0 in the center row, it can move down """
+    zeroInCentreRow = 4
+    g = Generator()
+
+    canMoveDown = g.canZeroMoveDown(zeroInCentreRow)
+    self.assertEqual(canMoveDown, True)
+
+  def testIfStateCanMoveZeroRight(self):
+    """ 012345678 has 0 at the left corner, it can move right"""
+    zeroInLeftCorner = 0
+    g = Generator()
+
+    canMoveRight = g.canZeroMoveRight(zeroInLeftCorner)
+    self.assertEqual(canMoveRight, True)
+
+  def testIfStateCanMoveZeroLeft(self):
+    """ 120345678 has 0 at the right corner, it can move left"""
+    zeroInRightCorner = 2
+    g = Generator()
+
+    canMoveLeft = g.canZeroMoveLeft(zeroInRightCorner)
+    self.assertEqual(canMoveLeft, True)
+
+  def testMoveZeroUp(self):
+    """ swap 0 and 2 """
+    current  = "123405678"
+    expected = "103425678"
+
+    g = Generator()
+    state = g.moveZeroUp(current)
+    self.assertEqual(state, expected)
+
+  def testMoveZeroDown(self):
+    """ swap 0 and 8 """
+    current  = "123450678"
+    expected = "123458670"
+
+    g = Generator()
+    state = g.moveZeroDown(current)
+    self.assertEqual(state, expected)
+
+  def testMoveZeroRight(self):
+    current  = "012345678"
+    expected = "102345678"
+
+    state = Generator().moveZeroRight(current)
+    self.assertEqual(state, expected)
+
+  def testMoveZeroLeft(self):
+    current  = "123450678"
+    expected = "123405678"
+
+    state = Generator().moveZeroLeft(current)
+    self.assertEqual(state, expected)
+
+if __name__ == '__main__':
+  unittest.main()
