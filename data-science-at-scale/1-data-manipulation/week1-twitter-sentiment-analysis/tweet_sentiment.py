e32934feb302cb01556a3d71a2d1b0eee4be09bc
Rafael Portela <rafaelportela90@gmail.com>
2015-11-13 13:23:28 -0200

    Problem 2: print sentiments of input tweets

--- data-science-at-scale/1-data-manipulation/week1-twitter-sentiment-analysis/tweet_sentiment.py
+++ data-science-at-scale/1-data-manipulation/week1-twitter-sentiment-analysis/tweet_sentiment.py
@@ -1,17 +1,43 @@
 import sys
+import json
+import pdb
 
-def hw():
-    print 'Hello, world!'
+def read_sentiments(fp):
+    dict = {}
+    for line in fp:
+        term, score = line.split("\t")
+        dict[term] = int(score)
 
-def lines(fp):
-    print str(len(fp.readlines()))
+    return dict
+
+def read_tweets(fp):
+    tweets = []
+    for line in fp:
+        tweets.append(json.loads(line))
+
+    return tweets
+
+def tweet_sentiment_number(tweet, sentiments):
+    sentiment_sum = 0
+    words = tweet['text'].split(' ')
+    for word in words:
+        if word in sentiments:
+            sentiment_sum += sentiments[word]
+    return sentiment_sum
+
+def print_tweets_sentiment(tweets, sentiments):
+    for tweet in tweets:
+        if 'text' in tweet:
+            print tweet_sentiment_number(tweet, sentiments)
 
 def main():
     sent_file = open(sys.argv[1])
+    sentiments = read_sentiments(sent_file)
+
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    tweets = read_tweets(tweet_file)
+
+    print_tweets_sentiment(tweets, sentiments)
 
 if __name__ == '__main__':
     main()
